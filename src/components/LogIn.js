8448fcad3489bfa1352ae3cb2e63e85ec1917c17
Evan Kwan <evanyuekwan@gmail.com>
2021-07-29 13:07:42 -0400

    added useState and google auth

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,8 +1,25 @@
 import firebase from '../config/firebase';
+import 'firebase/auth';
+import { useState } from 'react';
 
 const LogIn = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
   firebase.auth().signInWithRedirect(provider);
+  firebase.auth()
+    .getRedirectResult()
+    .then((result) => {
+      if (result.credential) {
+            /** @type {firebase.auth.OAuthCredential} */
+            const credential = result.credential;
+
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const token = credential.accessToken;
+            // ...
+          }
+          // The signed-in user info.
+          const user = result.user;
+          console.log(result);
+    })
 
   return (
     <>
