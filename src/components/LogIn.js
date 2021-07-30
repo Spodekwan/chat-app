b1faf50bc269e6ddcec75d68b108e8b7221cbcc9
Jake Spodek <jake.spodek1@gmail.com>
2021-07-30 12:22:39 -0400

    authentication fix

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -24,8 +24,8 @@ const LogIn = () => {
     getRedirectResult(auth)
       .then((result) => {
         // This gives you a Google Access Token. You can use it to access Google APIs.
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
+        // const credential = GoogleAuthProvider.credentialFromResult(result);
+        // const token = credential.accessToken;
         console.log(result);
 
         // The signed-in user info.
@@ -34,15 +34,15 @@ const LogIn = () => {
 
       }).catch((error) => {
         // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
+        // const errorCode = error.code;
+        // const errorMessage = error.message;
         // The email of the user's account used.
-        const email = error.email;
+        // const email = error.email;
         // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
+        // const credential = GoogleAuthProvider.credentialFromError(error);
         // ...
       });
-  }, []);
+  }, [auth, history, setUser]);
 
   return (
     <>
