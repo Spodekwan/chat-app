280ae3f852a200559ec21c9db3ca9771215db9ce
Jake Spodek <jake.spodek1@gmail.com>
2021-10-29 16:26:45 -0400

    cleanup

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -55,12 +55,7 @@ const LogIn = () => {
   useEffect(() => {
     getRedirectResult(auth)
       .then((result) => {
-        // This gives you a Google Access Token. You can use it to access Google APIs.
-        // const credential = GoogleAuthProvider.credentialFromResult(result);
-        // const token = credential.accessToken;
         console.log('setting user', result, user);
-
-        // The signed-in user info.
         // setUser(result.user);
 
         const db = getDatabase();
@@ -80,13 +75,6 @@ const LogIn = () => {
           history.push('/rooms');
         }
       }).catch((error) => {
-        // Handle Errors here.
-        // const errorCode = error.code;
-        // const errorMessage = error.message;
-        // The email of the user's account used.
-        // const email = error.email;
-        // The AuthCredential type that was used.
-        // const credential = GoogleAuthProvider.credentialFromError(error);
         // ...
       });
   }, [auth, history, setUser]);
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -55,12 +55,9 @@ const Profile = () => {
   const { user } = useAuth();
   const history = useHistory();
   const [imageURL, setImageURL ] = useState('');
-  // const [userImageDisplay, setUserImageDisplay] = useState(`${user.photoURL}`);
 
   const db = getDatabase();
   const currentUser = ref(db, `Users/${user.uid}`);
-
-  // https://lh3.googleusercontent.com/a/AATXAJxsQ43tqs0lEb41X-AooViYQgpbCvgKRuaH2Gk=s96-c
 
   const handleImageUpdate = (event) => {
     event.preventDefault();
@@ -74,32 +71,10 @@ const Profile = () => {
       console.log(user.displayName, user.photoURL);
       user.photoURL = imageURL;
       console.log(user.displayName, user.photoURL);
-      
-      // setUserImageDisplay(imageURL);
 
       setImageURL('');
     }
   }
-
-  // get(currentUser)
-  //   .then((snapshot) => {
-  //       const data = snapshot.val();
-  //       console.log(data.photoURL);
-  // });
-
-  // useEffect(() => {
-  //   const db = getDatabase();
-  //   const currentUser = ref(db, `Users/${user.uid}`);
-  //   onValue(currentUser, (snapshot) => {
-  //     const data = snapshot.val();
-  //     console.log(data.photoURL);
-  //   });
-  // }, [])
-
-  // onValue(currentUser, (snapshot) => {
-  //     const data = snapshot.val();
-  //     console.log(data.photoURL);
-  // });
 
   return (
     <>
@@ -109,21 +84,8 @@ const Profile = () => {
           <BackButton to="/rooms">Back</BackButton>
           <PageTitle>Your Profile</PageTitle>
           <ProfileImageContainer>
-            {/* <ProfileImage src={userImageDisplay} alt="your profile picture"></ProfileImage> */}
             <ProfileImage src={user.photoURL} alt="your profile picture"></ProfileImage>
           </ProfileImageContainer>
-          {/* <form action="submit" onSubmit={(event) => handleImageUpdate(event)}>
-            <label htmlFor="profileImage">Import profile image from URL: </label>
-            <input 
-              type="text" 
-              id="profileImage"
-              accept="image/png, image/jpeg"
-              value={imageURL}
-              onChange={(event) => setImageURL(event.target.value)}
-              required
-            />
-            <button type="submit">Update</button>
-          </form> */}
           <DisplayName>Username: {user.displayName}</DisplayName>
           <p className="tempMessage">Ability to update profile pictures coming soon!</p>
         </Wrapper>
